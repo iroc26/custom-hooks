6cd8559081c05ff1a411926e61142c811feadba7
Coral <iroc2526@gmail.com>
2025-04-16 18:35:14 -0600

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+
+const localCache = {};
+
+
+export const useFetch = ( url ) =>{
+
+    const [state, setState ] = useState({
+        data: null,
+        isLoading: true,
+        hasError: false,
+        error: null,
+    });
+
+    useEffect(() =>{
+        getFetch();
+    }, [url]);
+
+    const setLoadingState = () =>{
+        setState({
+            data: null,
+            isLoading:true,
+            hasErrror:false,
+            error:null,
+        });
+    }
+
+    const getFetch = async() =>{
+
+        if( localCache[url] ){
+            console.log('usando cache');
+
+            setState({
+                data: localCache[url],
+                isLoading: false,
+                hasError: false,
+                erro: null,
+            });
+            return;
+        }
+
+        setLoadingState();
+        const resp = await fetch( url );
+
+        // sleep
+        await new Promise( resolve => setTimeout(resolve, 2000));
+        
+        if( !resp.ok ){
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error:{
+                    code: resp.status,
+                    massage: resp.statusText,
+                }
+            });
+            return;
+
+        }
+
+        const data = await resp.json();
+
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            erro: null,
+        });
+        
+        // Manejo del caché
+        localCache[url] = data;
+
+    }
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    }
+}
